7ee153dc059cece4403085dc2b527e7a43285793
XFernando43 <U20171E997@upc.edu.pe>
2024-05-04 15:47:24 -0500

    add: auto import components in primevue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+
+import './style.css';
+// import 'primevue/resources/themes/aura-light-teal/theme.css';
+// import 'primevue/resources/themes/lara-dark-pink/theme.css'
+// import 'primevue/resources/themes/md-light-indigo/theme.css'
+// import 'primevue/resources/themes/lara-dark-purple/theme.css'
+// import 'primevue/resources/themes/lara-dark-teal/theme.css'
+// import 'primevue/resources/themes/aura-light-green/theme.css';
+import 'primevue/resources/themes/lara-dark-green/theme.css';
+
+
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
+app.use(PrimeVue)
+
 app.use(router)
 app.mount('#app')
 
--- src/views/loginPage.vue
+++ src/views/loginPage.vue
@@ -1,14 +1,18 @@
 <template>
-
-    <div class="container">
-
-        <div class="flex flex-col gap-2 justify-center">
-            <img src="../assets/329182497_580658887303089_6817430973110470755_n.jpg" class=" max-w-12">
-
-        </div>
-
+     <div class="login-form">
+    <h2>Iniciar sesión</h2>
+    <div class="p-fluid">
+      <div class="p-field">
+        <label for="username">Nombre de usuario</label>
+        <InputText id="username" />
+      </div>
+      <div class="p-field">
+        <label for="password">Contraseña</label>
+        <Password  toggleMask />
+      </div>
+      <Button label="Iniciar sesión" class="p-button-primary"/>
     </div>
-
+  </div>
 </template>
 
 <script lang="ts">
